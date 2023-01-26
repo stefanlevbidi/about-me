2300e62d037f67cd3b5c3b89097e388b6f6b9c3f
Andrew Morris <andrew.morris.crypto@gmail.com>
2023-01-26 09:50:13 +0000

    Add functions

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 "use strict";
+function dogQuestion() {
+    let question1 = confirm("Do you have a dog")
+    console.log(question1)
 
-let question1 = confirm("Do you have a dog")
-console.log(question1)
-
-if(question1 == true) {
-    console.log("Let's get ready to RUMBLE!")
-}else {
-    console.log("Ah well")
+    if (question1 == true) {
+        console.log("Let's get ready to RUMBLE!")
+    } else {
+        console.log("Ah well")
+    }
 }
 
-let colour = prompt("What is your favourite color?")
 
-switch(colour) {
-case "red":
-    console.log("Your favourite color was red");
-    break;
-case "green":
-    console.log("Nice, Good, choice");
-    break;
-default:
-    console.log("Pretty sure that color doesn't exist. Have you thought about red or green?");
+function colourQuestion() {
+    let colour = prompt("What is your favourite color?")
+
+    switch (colour) {
+        case "red":
+            console.log("Your favourite color was red");
+            break;
+        case "green":
+            console.log("Nice, Good, choice");
+            break;
+        default:
+            console.log("Pretty sure that color doesn't exist. Have you thought about red or green?");
+    }
 }
